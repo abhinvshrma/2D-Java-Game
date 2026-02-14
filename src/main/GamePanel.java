84526cdd49a3133cf34878b5f05e1cc8a13213ef
abhinvshrma <abhisharma.dev0@gmail.com>
2026-02-14 09:22:21 +0530

    Inital commit : 2D Java Game

---
+++ src/main/GamePanel.java
@@ -0,0 +1,111 @@
+package main;
+
+import entity.Player;
+import object.SuperObject;
+import tile.TileManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    // SCREEN SETTINGS
+    final int originalTileSize = 16;
+    final int scale  = 3;
+
+    public final int tileSize = originalTileSize * scale; // 16*3=48pixels
+    public final int maxScreenColumn = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenColumn;
+    public final int screenHeight = tileSize * maxScreenRow;
+
+    //WORLD SETTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+
+    final int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+
+    Sound music = new Sound();
+    Sound se = new Sound();
+
+    Thread gameThread;
+    public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
+
+    //ENTITY AND OBJECT
+    public Player player = new Player(this,keyH);
+    public SuperObject obj[] = new SuperObject[10];
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+    public void setupGame(){
+        aSetter.setObject();
+
+        playMusic(0);
+    }
+    public void startGameThread () {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            while (delta >= 1) {  // Ensures only 1 update per frame
+                update();
+                repaint();
+                delta--;
+            }
+
+        }
+
+    }
+    public void update() {
+        player.update();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        // tile
+        tileM.draw(g2);
+        // object
+        for(int i =0 ; i < obj.length ; i ++){
+            if(obj[i] != null){
+                obj[i].draw(g2 , this);
+            }
+        }
+        // Player
+        player.draw(g2);
+
+        //UI
+        ui.draw(g2);
+
+    }
+    public void playMusic(int i){
+        music.setFile(i);
+        music.play();
+        music.loop();
+    }
+    public void stopMusic(){
+        music.stop();
+    }
+    public void playSE(int i){
+        se.setFile(i);
+        se.play();
+    }
+}
